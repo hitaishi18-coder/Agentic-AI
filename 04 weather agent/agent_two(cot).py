71765851f22dc54c802bd7aef4f3546292abc0ce
Hitaishi Lohtia <hitaishi.8.8.8@gmail.com>
2026-03-03 22:36:22 +0530

    agentic ai

---
+++ 04 weather agent/agent_two(cot).py
@@ -0,0 +1,126 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+import requests
+from pydantic import BaseModel, Field
+from typing import Optional
+import json
+
+load_dotenv()
+
+client = OpenAI()
+
+
+def get_weather(city: str):
+    geo_url = f"https://geocoding-api.open-meteo.com/v1/search?name={city}&count=1"
+    geo_response = requests.get(geo_url).json()
+
+    if "results" not in geo_response:
+        return "City not found"
+
+    lat = geo_response["results"][0]["latitude"]
+    lon = geo_response["results"][0]["longitude"]
+
+    weather_url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
+    weather_response = requests.get(weather_url).json()
+
+    if "current_weather" in weather_response:
+        temp = weather_response["current_weather"]["temperature"]
+        wind = weather_response["current_weather"]["windspeed"]
+        return f"The weather in {city} is {temp}°C with wind speed {wind} km/h"
+
+    return "Something went wrong"
+
+available_tools = {
+    "get_weather": get_weather
+}
+
+
+SYSTEM_PROMPT = """
+    You're an expert AI Assistant in resolving user queries using chain of thought.
+    You work on START, PLAN and OUPUT steps.
+    You need to first PLAN what needs to be done. The PLAN can be multiple steps.
+    Once you think enough PLAN has been done, finally you can give an OUTPUT.
+    You can also call a tool if required from the list of available tools.
+    for every tool call wait for the observe step which is the output from the called tool.
+
+    Rules:
+    - Strictly Follow the given JSON output format
+    - Only run one step at a time.
+    - The sequence of steps is START (where user gives an input), PLAN (That can be multiple times) and finally OUTPUT (which is going to the displayed to the user).
+
+    Output JSON Format:
+    { "step": "START" | "PLAN" | "OUTPUT" | "TOOL", "content": "string", "tool": "string", "input": "string" }
+
+    Available Tools:
+    - get_weather(city: str): Takes city name as an input string and returns the weather info about the city.
+    - run_command(cmd: str): Takes a system linux command as string and executes the command on user's system and returns the output from that command
+    
+    Example:
+    START: What is the weather of Delhi?
+    PLAN: { "step": "PLAN": "content": "Seems like user is interested in getting weather of Delhi in India" }
+    PLAN: { "step": "PLAN": "content": "Lets see if we have any available tool from the list of available tools" }
+    PLAN: { "step": "PLAN": "content": "Great, we have get_weather tool available for this query." }
+    PLAN: { "step": "PLAN": "content": "I need to call get_weather tool for delhi as input for city" }
+    PLAN: { "step": "TOOL": "tool": "get_weather", "input": "delhi" }
+    PLAN: { "step": "OBSERVE": "tool": "get_weather", "output": "The temp of delhi is cloudy with 20 C" }
+    PLAN: { "step": "PLAN": "content": "Great, I got the weather info about delhi" }
+    OUTPUT: { "step": "OUTPUT": "content": "The cuurent weather in delhi is 20 C with some cloudy sky." }
+    
+"""
+
+print("\n\n\n")
+
+class MyOutputFormat(BaseModel):
+    step: str = Field(..., description="The ID of the step. Example: PLAN, OUTPUT, TOOL, etc")
+    content: Optional[str] = Field(None, description="The optional string content for the step")
+    tool: Optional[str] = Field(None, description="The ID of the tool to call.")
+    input: Optional[str] = Field(None, description="The input params for the tool")
+
+message_history = [
+    { "role": "system", "content": SYSTEM_PROMPT },
+]
+
+while True:
+    user_query = input("👉🏻 ")
+    message_history.append({ "role": "user", "content": user_query })
+
+    while True:
+        response = client.chat.completions.parse(
+            model="gpt-4o",
+            response_format=MyOutputFormat,
+            messages=message_history,
+            max_tokens=1000
+        )
+
+        raw_result = response.choices[0].message.content
+        message_history.append({"role": "assistant", "content": raw_result})
+        
+        parsed_result = response.choices[0].message.parsed
+
+        if parsed_result.step == "START":
+            print("🔥", parsed_result.content)
+            continue
+
+        if parsed_result.step == "TOOL":
+            tool_to_call = parsed_result.tool
+            tool_input = parsed_result.input
+            print(f"🛠️: {tool_to_call} ({tool_input})")
+
+            tool_response = available_tools[tool_to_call](tool_input)
+            print(f"🛠️: {tool_to_call} ({tool_input}) = {tool_response}")
+            message_history.append({ "role": "developer", "content": json.dumps(
+                { "step": "OBSERVE", "tool": tool_to_call, "input": tool_input, "output": tool_response}
+            ) })
+            continue
+
+
+
+        if parsed_result.step == "PLAN":
+            print("🧠", parsed_result.content)
+            continue
+
+        if parsed_result.step == "OUTPUT":
+            print("🤖", parsed_result.content)
+            break
+
+print("\n\n\n")
